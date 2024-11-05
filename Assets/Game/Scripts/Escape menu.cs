3a6f3693b9df606eef38416cfd559f117236f843
Confsuin <Lukas.bottiger@icloud.com>
2024-11-05 10:23:58 +0100

    Menu updates

--- Assets/Game/Scripts/Escape menu.cs
+++ Assets/Game/Scripts/Escape menu.cs
@@ -9,7 +9,8 @@ public class Escapemenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.localScale = new Vector2(0, 0);
+        GameObject.Find("Escape Menu Buttons").transform.localScale = new Vector2(0, 0);
+        GameObject.Find("OptionsMenu").transform.localScale = new Vector2(0, 0);
         GameObject.Find("Image").transform.localScale = new Vector2(0, 0);
     }
 
@@ -20,22 +21,16 @@ public class Escapemenu : MonoBehaviour
         {
             if (EscapeMenuActive == false)
             {
-                GameObject.Find("Resume").transform.localScale = new Vector2(3, 3);
-                GameObject.Find("Options").transform.localScale = new Vector2(3, 3);
-                GameObject.Find("Exit").transform.localScale = new Vector2(3, 3);
-                GameObject.Find("Image").transform.localScale = new Vector2(3, 3);
-                GameObject.Find("Main Menu").transform.localScale = new Vector2(3, 3);
+                GameObject.Find("Escape Menu Buttons").transform.localScale = new Vector2(1, 1);
+                GameObject.Find("Image").transform.localScale = new Vector2(1, 1);
                 EscapeMenuActive = true;
                 Time.timeScale = 0;
             }
             else
             {
-                GameObject.Find("Resume").transform.localScale = new Vector2(0, 0);
-                GameObject.Find("Options").transform.localScale = new Vector2(0, 0);
-                GameObject.Find("Exit").transform.localScale = new Vector2(0, 0);
+                GameObject.Find("Escape Menu Buttons").transform.localScale = new Vector2(0, 0);
                 GameObject.Find("Image").transform.localScale = new Vector2(0, 0);
-                GameObject.Find("Button").transform.localScale = new Vector2(0, 0);
-                GameObject.Find("Main Menu").transform.localScale = new Vector2(0, 0);
+                GameObject.Find("OptionsMenu").transform.localScale = new Vector2(0, 0);
                 EscapeMenuActive = false;
                 Time.timeScale = 1;
             }
@@ -45,21 +40,15 @@ public class Escapemenu : MonoBehaviour
     }
     public void doResumeGame()
     {
-        GameObject.Find("Resume").transform.localScale = new Vector2(0, 0);
-        GameObject.Find("Options").transform.localScale = new Vector2(0, 0);
-        GameObject.Find("Exit").transform.localScale = new Vector2(0, 0);
+        GameObject.Find("Escape Menu Buttons").transform.localScale = new Vector2(0, 0);
         GameObject.Find("Image").transform.localScale = new Vector2(0, 0);
-        GameObject.Find("Main Menu").transform.localScale = new Vector2(0, 0);
         EscapeMenuActive = false;
         Time.timeScale = 1;
     }
     public void doOptions()
     {
-        GameObject.Find("Resume").transform.localScale = new Vector2(0, 0);
-        GameObject.Find("Options").transform.localScale = new Vector2(0, 0);
-        GameObject.Find("Exit").transform.localScale = new Vector2(0, 0);
-        GameObject.Find("Main Menu").transform.localScale = new Vector2(0, 0);
-        GameObject.Find("Button").transform.localScale = new Vector2(3, 3);
+        GameObject.Find("Escape Menu Buttons").transform.localScale = new Vector2(0, 0);
+        GameObject.Find("OptionsMenu").transform.localScale = new Vector2(1, 1);
     }
     public void doExitGame()
     {
